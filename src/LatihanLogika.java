ecd80660aeea55279633ca09faf072702ea6453d
M Ikhlas N R <72897730+M-Ikhlas-N-R@users.noreply.github.com>
2020-10-16 09:25:32 +0700

    Add files via upload

---
+++ src/LatihanLogika.java
@@ -0,0 +1,17 @@
+/*
+ * @author
+ * Nama              : M Ikhlas N R
+ * Kelas             : IF-1
+ * NIM               : 10119022
+ * Deskripsi Program : Latihan menggunakan logika
+ *
+ */
+
+public class LatihanLogika {
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+}
